a367f6fbf4d134071483baba76537d2c085bbf23
jullionetto-hub <jotaceene1987@gmail.com>
2026-06-22 08:15:29 -0300

    Atualizar dashboard - correcoes e melhorias

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Dashboard rodando na porta ' + (process.env.PORT || 3000));
+});
